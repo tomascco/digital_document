98ffaf64e591a86cfa581635dab92078585dc427
Tomás Coêlho <tomascoelho6@gmail.com>
2023-03-25 12:44:21 -0300

    Add tests

--- test/digital_document_test.rb
+++ test/digital_document_test.rb
@@ -6,8 +6,4 @@ class TestDigitalDocument < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::DigitalDocument::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
---
+++ test/pdf/save_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class DigitalDocument::PDF
+  class SaveTest < Minitest::Test
+    def test_pdf_creation_and_building
+      # Arrange
+      pdf = DigitalDocument::PDF.new([
+        {Type: :Catalog, Pages: DigitalDocument::PDF::Reference.new(2)},
+        {Type: :Pages, Kids: [DigitalDocument::PDF::Reference.new(3)], Count: 1},
+        {Type: :Page, MediaBox: [0, 0, 595.44, 841.68], Contents: DigitalDocument::PDF::Reference.new(4)},
+        DigitalDocument::PDF::Stream.new("1.0 0.0 0.0 RG\n0.5 0.75 1.0 rg\n97.72 220.84 400 400 re\nB"),
+        true,
+        false,
+        nil
+      ])
+      io = StringIO.new
+
+      # Act
+      Save.to_io(io, pdf:)
+
+      # Assert
+      io.rewind
+      File.open("test/support/fixture.pdf", "rb") do |fixture|
+        assert(FileUtils.compare_stream(fixture, io))
+      end
+    end
+  end
+end
